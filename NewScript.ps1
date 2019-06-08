0655a20d028fb02c0867b602a03b5aff5ae4d78c
Chase J <9084598+TheChaseJ@users.noreply.github.com>
2019-06-08 14:28:42 -0500

    Create NewScript.ps1

---
+++ NewScript.ps1
@@ -0,0 +1,13 @@
+
+<#	
+	.NOTES
+	===========================================================================================================
+  Created      :  Month 01, 2019
+  Updated      :  Month 01, 2019
+  Filename     :  File name
+  Autor        :  TheChaseJ (https://github.com/TheChaseJ)
+  Repository   :  https://github.com/TheChaseJ/"Link to file"
+	===========================================================================================================
+  .DESCRIPTION
+	Enter description of script here.
+#>
