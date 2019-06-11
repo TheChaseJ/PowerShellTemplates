d608bf28a5680acdffd0588532f8ecefb496d240
Chase J <9084598+TheChaseJ@users.noreply.github.com>
2019-06-11 10:21:56 -0500

    Update MsgBox.ps1

--- MsgBox.ps1
+++ MsgBox.ps1
@@ -1,5 +1,7 @@
 Add-Type -AssemblyName PresentationFramework
+# Above line only has to be run once at the top of script.
 
+# Block below line can be multiplied as many times as needed.
 $msgBoxInput =  [System.Windows.MessageBox]::Show('Text in box','Box title','YesNo','Question')
 
   switch  ($msgBoxInput) {
